61b8c61a8b8dd0dbcbc329f25729838202e51d9c
Student <jeff.kandel@gmail.com>
2017-04-10 09:35:21 -0400

    Don't leave it with errors

--- app/components/Campus/CampusDetail.jsx
+++ app/components/Campus/CampusDetail.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import List from '../List'
 
 /* -----------------    COMPONENT     ------------------ */
 
@@ -9,7 +8,7 @@ class CampusDetail extends Component {
   render() {
     return (
       <div>
-      detail
+        Coming Soon... Campus Details
       </div>
     )
   }
