cf2724e5cd9bb05e1553615cf7874d1ab1768ef3
Student <jeff.kandel@gmail.com>
2017-04-10 09:31:30 -0400

    Finished with this for now

--- app/components/Student/StudentDetail.jsx
+++ app/components/Student/StudentDetail.jsx
@@ -8,7 +8,7 @@ class StudentDetail extends Component {
   render() {
     return (
       <div>
-      detail
+      Coming Soon... Campus Details
       </div>
     )
   }
@@ -21,38 +21,3 @@ const mapState = ({ campuses }) => ({ campuses });
 const mapDispatch = {};
 
 export default connect(mapState, mapDispatch)(StudentDetail);
-
-// <div className="container">
-//   <div className="panel panel-warning">
-//     <div className="panel-heading">
-//       <h2 className="panel-title large-font">
-//       <span>Campuses</span>
-//       <i className="fa fa-plus pull-right" aria-hidden="true"></i>
-//       </h2>
-//     </div>
-//   </div>
-//   <div className="user-list">
-//   {
-//     campuses
-//       .map(campus => (
-//         <div key = {campus.id} className="list-group-item min-content user-item">
-//           <div className="media">
-//             <div className="media-left media-middle icon-container">
-//               <img className="media-object img-circle" src={campus.image} />
-//             </div>
-//             <Link
-//               className="media-body"
-//               activeClassName="active"
-//               to={`/campuses/${campus.id}`}>
-//               <h4 className="media-heading tucked">
-//                 <span placeholder="Jean Doe">{campus.name}</span>
-//               </h4>
-//             </Link>
-//             <div className="media-right media-middle">
-//             </div>
-//           </div>
-//         </div>)
-//       )
-//   }
-//   </div>
-// </div>
